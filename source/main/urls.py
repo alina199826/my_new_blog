fbaf0705ebde6600669f07798f259b10396f122f
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2023-01-31 21:08:13 +0600

    Результат занятия 69

--- source/main/urls.py
+++ source/main/urls.py
@@ -18,8 +18,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+api_url = [
+    path('v1/', include('api_v1.urls'))
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
     path('accounts/', include('accounts.urls')),
+    path('api/', include(api_url)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
